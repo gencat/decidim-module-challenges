13b76d1e2c1dd442a01e33cb61d5c5566621db44
Ramon Costa <ramon.costa@gmail.com>
2020-10-26 15:45:47 +0100

    Clean model code

--- app/models/decidim/challenges/challenge.rb
+++ app/models/decidim/challenges/challenge.rb
@@ -4,10 +4,9 @@ module Decidim
   module Challenges
     # The data store for a Challenge in the Decidim::Challenges component.
     class Challenge < Challenges::ApplicationRecord
-      include Decidim::Resourceable
       include Decidim::HasComponent
+      include Decidim::Resourceable
       include Decidim::ScopableComponent
-      # include Decidim::Searchable
       include Decidim::Traceable
       include Decidim::TranslatableAttributes
       # include Decidim::TranslatableResource
@@ -17,30 +16,8 @@ module Decidim
       VALID_STATES = %i[proposal executing finished].freeze
       enum state: VALID_STATES
 
-      # belongs_to :area,
-      #            foreign_key: "decidim_area_id",
-      #            class_name: "Decidim::Area",
-      #            optional: true
-
       component_manifest_name 'challenges'
 
-      # TODO
-      # searchable_fields({
-      #                    scope_id: :decidim_scope_id,
-      #                    participatory_space: :itself,
-      #                    A: :title,
-      #                    B: :local_description,
-      #                    C: :global_description,
-      #                    D: '', # TODO
-      #                    datetime: :published_at
-      #                  },
-      #                  index_on_create: ->(_process) { false },
-      #                  index_on_update: ->(process) { process.visible? })
-      #
-      # # Allow ransacker to search for a key in a hstore column (`title`.`en`)
-      # ransacker :title do |parent|
-      #  Arel::Nodes::InfixOperation.new("->>", parent.table[:title], Arel::Nodes.build_quoted(I18n.locale.to_s))
-      # end
     end
   end
 end
