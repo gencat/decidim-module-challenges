994fd57858b59e400533c1fc7c9885e459562d7e
Ramon Costa <ramon.costa@gmail.com>
2020-10-28 12:20:06 +0100

    Fix challenge command rspec test with trace log

--- spec/commands/decidim/challenges/admin/destroy_challenge_spec.rb
+++ spec/commands/decidim/challenges/admin/destroy_challenge_spec.rb
@@ -9,9 +9,9 @@ module Decidim
         subject { described_class.new(challenge, current_user) }
 
         let(:my_process) { create :participatory_process }
-        let(:challenge) { create :challenge }
         let!(:current_user) { create :user, email: 'some_email@example.org', organization: my_process.organization }
         let!(:user) { create :user, :confirmed, organization: my_process.organization }
+        let(:challenge) { create :challenge }
 
         context 'when everything is ok' do
           it 'deletes the challenge' do
@@ -19,14 +19,13 @@ module Decidim
             expect { challenge.reload }.to raise_error(ActiveRecord::RecordNotFound)
           end
 
-          it 'traces the action' do
+          it 'traces the action', versioning: true do
             expect(Decidim.traceability)
               .to receive(:perform_action!)
               .with('delete', challenge, current_user)
               .and_call_original
 
             expect { subject.call }.to change(Decidim::ActionLog, :count)
-
             action_log = Decidim::ActionLog.last
             expect(action_log.version).to be_present
             expect(action_log.version.event).to eq 'destroy'
