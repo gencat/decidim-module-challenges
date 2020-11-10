b6b6a7fccd374399caf7cc0d83d72b279bda41d8
Uriel Huertas <uriel.huertas@dxcfds.com>
2020-11-10 20:10:39 +0100

    Updated sdgs spec examples.

--- spec/system/decidim/sdgs/browse_sdgs_spec.rb
+++ spec/system/decidim/sdgs/browse_sdgs_spec.rb
@@ -6,25 +6,8 @@ describe "Public Sustainable Development Goals", type: :system do
   include_context "with a component"
 
   let(:manifest_name) { "sdgs" }
-  let(:sdgs_names) { [
-    "(1) No Poverty",
-    "(2) Zero Hunger",
-    "(3) Good Health and Well-being",
-    "(4) Quality Education",
-    "(5) Gender Equality",
-    "(6) Clean Water and Sanitation",
-    "(7) Affordable and Clean Energy",
-    "(8) Decent Work and Economic Growth",
-    "(9) Industry Innovation and Infrastructure",
-    "(10) Reducing Inequality",
-    "(11) Sustainable Cities and Communities",
-    "(12) Responsible Consumption and Production",
-    "(13) Climate Action",
-    "(14) Life Below Water",
-    "(15) Life On Land",
-    "(16) Peace Justice and Strong Institutions",
-    "(17) Partnerships for the Goals",
-  ] }
+  let(:ods_ids) { [*1..17].map! { |n| "#ods-#{sprintf '%02d', n}"} }
+  let(:objective_ids) { [*1..17].map! { |n| "#objective_#{sprintf '%02d', n}"} }
 
   before do
     switch_to_host(organization.host)
@@ -37,8 +20,25 @@ describe "Public Sustainable Development Goals", type: :system do
       end
 
       it "shows the list of all SDGs" do
-        sdgs_names.each do |name|
-          expect(page).to have_content(name)
+        expect(page).to have_selector('.ods', count: 18)
+      end
+
+      it "has all the objective texts" do
+        container = page.find("#objective_container")
+
+        objective_ids.each do |objective|
+          expect(container).to have_selector(objective)
+        end
+      end
+
+      describe "ods logo click" do
+        context "when a ODS logo is clicked" do
+          it "should display an element containing the objective explanation after clicking its logo" do
+            for i in [0..16] do
+              page.find(ods_ids[i]).click
+              expect(page).to have_css(objective_ids[i], visible: true)
+            end
+          end
         end
       end
     end
