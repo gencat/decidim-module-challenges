169ca9927fb4a43ae07c3476749757fe86e42080
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2020-11-17 22:07:47 +0100

    Extract sdgs filter control into a helper

--- app/helpers/decidim/sdgs/sdgs_helper.rb
+++ app/helpers/decidim/sdgs/sdgs_helper.rb
@@ -5,6 +5,10 @@ module Decidim
     # Custom helpers, scoped to the Sdgs engine.
     #
     module SdgsHelper
+      def sdgs_filter_selector(form)
+        render partial: "decidim/sdgs/sdgs_filter/filter_selector", locals: {form: form}
+      end
+
       def t_sdg(name)
         t(name, scope: "decidim.sdgs.names")
       end
