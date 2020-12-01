ac6e372fb2efae7215d0fca8540bf6b00ec16f39
spuigmitja <Sergi.Puigmitja@dxcfds.com>
2020-12-01 16:06:17 +0100

    add tests and tweaks on challenges
    (cherry picked from commit 1e1624565ec99a2356d5da1482aff80a29b76287)

--- spec/cells/decidim/problems/problem_cell_spec.rb
+++ spec/cells/decidim/problems/problem_cell_spec.rb
@@ -6,12 +6,34 @@ module Decidim::Problems
   describe ProblemCell, type: :cell do
     controller Decidim::Problems::ProblemsController
 
-    let!(:problem) { create(:problem) }
+    let(:description) do
+      Decidim::Faker::Localized.sentence(3)
+    end
+    let!(:problem) { create :problem, description: description }
+    let(:model) { problem }
+    let!(:problem_title) { translated(problem.title) }
+    let!(:problem_description) { translated(problem.description) }
+    let!(:challenge_title) { translated(problem.challenge.title) }
+    let(:html) { cell("decidim/problems/problem", problem, context: { show_space: show_space }).call }
 
     context "when rendering" do
+
+      let(:show_space) { false }
+
       it "renders the card" do
-        html = cell("decidim/problems/problem", problem).call
         expect(html).to have_css(".card--problem")
+      end
+
+      it "renders the problem title" do
+        expect(html).to have_content(problem_title)
+      end
+
+      it "renders the problem description" do
+        expect(html).to have_content(problem_description)
+      end
+
+      it "renders the challenge title" do
+        expect(html).to have_content(challenge_title)
       end
     end
   end
