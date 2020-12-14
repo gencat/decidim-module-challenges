cc18b13ea9315514c8cb1c5a03b3c2c4372fde51
spuigmitja <Sergi.Puigmitja@dxcfds.com>
2020-12-14 16:28:22 +0100

    fix offenses

--- app/cells/decidim/solutions/solution_cell.rb
+++ app/cells/decidim/solutions/solution_cell.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 module Decidim
-    module Solutions
-      class SolutionCell < Decidim::ViewModel
-        include SolutionCellsHelper
-        include Decidim::SanitizeHelper
-        include Cell::ViewModel::Partial
-  
-        def show
-          cell card_size, model, options
-        end
-  
-        private
-  
-        def card_size
-          "decidim/solutions/solution_m"
-        end
+  module Solutions
+    class SolutionCell < Decidim::ViewModel
+      include SolutionCellsHelper
+      include Decidim::SanitizeHelper
+      include Cell::ViewModel::Partial
+
+      def show
+        cell card_size, model, options
+      end
+
+      private
+
+      def card_size
+        "decidim/solutions/solution_m"
       end
     end
   end
+end
