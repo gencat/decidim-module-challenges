c064973ff0f8c36444e224523edca502cc2f036e
spuigmitja <Sergi.Puigmitja@dxcfds.com>
2020-12-14 12:22:36 +0100

    tweaks

--- app/assets/config/decidim_challenges_manifest.js
+++ app/assets/config/decidim_challenges_manifest.js
@@ -1,5 +1,4 @@
 //= link decidim/challenges/add_challenge.js
 //= link decidim/challenges/admin/challenges_form.js
 //= link_directory ../stylesheets/decidim/challenges .css
-//= link_tree ../images/decidim
 //= link_directory ../stylesheets/decidim/problems .css
