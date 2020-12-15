f665c54ef22f113d02f260960073f49bcf850006
Sergi Puigmitja <64258984+spuigmitjadxc@users.noreply.github.com>
2020-12-15 15:49:15 +0100

    Feature/problems public part (#17)
    * add problem cells
    
    * add show
    
    * fix lint offenses
    
    (cherry picked from commit 9cc02963e27469dfc464a03cbb709d23512260a3)
    
    * tweaks
    
    (cherry picked from commit 80f78ad89433f4929d2111ea5775b67a5614d418)
    
    * add tests and tweaks on challenges
    
    (cherry picked from commit 1e1624565ec99a2356d5da1482aff80a29b76287)
    
    * fix offenses
    
    (cherry picked from commit f501c0c0ada693dd8b1161443b7fe78046def632)
    
    * tweaks
    
    (cherry picked from commit 2c5747803d3132b12e257f3b43c6218d08631f07)
    
    * add sectorial and technologic fields on show and some tweaks
    
    * lint tweaks
    
    (cherry picked from commit 50a80ba86d438c94e18d12697fb8f8fda3dc4f7d)
    
    * tweaks
    
    * delete duplicate close of div

---
+++ app/cells/decidim/problems/problem_cell.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Problems
+    # This cell renders the problem card for an instance of a problem
+    # the default size is the Medium Card (:m)
+    class ProblemCell < Decidim::ViewModel
+      include Cell::ViewModel::Partial
+
+      def show
+        cell card_size, model, options
+      end
+
+      def card_size
+        "decidim/problems/problem_m"
+      end
+    end
+  end
+end
