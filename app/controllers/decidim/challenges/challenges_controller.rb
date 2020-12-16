0fab9fa1e9f7573e8cf11f2d40d39b57d07d6562
spuigmitja <Sergi.Puigmitja@dxcfds.com>
2020-12-16 15:40:24 +0100

    catch sdg null error on challenge

--- app/controllers/decidim/challenges/challenges_controller.rb
+++ app/controllers/decidim/challenges/challenges_controller.rb
@@ -21,8 +21,6 @@ module Decidim
 
       def show
         @challenge = Challenge.find(params[:id])
-        @sdg = @challenge.sdg_code
-        @sdg_index = (1 + Decidim::Sdgs::Sdg.index_from_code(@challenge.sdg_code.to_sym)).to_s.rjust(2, "0")
       end
 
       private
