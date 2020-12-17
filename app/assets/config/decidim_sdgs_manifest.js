3082d23a839960cbdd20fb65560af612837cbee3
spuigmitja <Sergi.Puigmitja@dxcfds.com>
2020-12-17 09:45:17 +0100

    delete unused js from manifest files

--- app/assets/config/decidim_sdgs_manifest.js
+++
@@ -1,2 +0,0 @@
-//= link decidim/challenges/add_challenge.js
-//= link decidim/challenges/admin/challenges_form.js
--- app/assets/config/decidim_solutions_manifest.js
+++ app/assets/config/decidim_solutions_manifest.js
@@ -1,3 +1,1 @@
-//= link decidim/challenges/add_challenge.js
-//= link decidim/challenges/admin/challenges_form.js
 //= link_directory ../stylesheets/decidim/solutions .css 
