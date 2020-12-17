aaac349eede9d58afc8a871869ec1bb34fad71ec
Sergi Puigmitja <64258984+spuigmitjadxc@users.noreply.github.com>
2020-12-17 08:55:17 +0100

    fix manifest importing extra css (#21)
    Co-authored-by: Oliver Valls <199462+tramuntanal@users.noreply.github.com>

--- lib/decidim/solutions/engine.rb
+++ lib/decidim/solutions/engine.rb
@@ -19,7 +19,7 @@ module Decidim
       end
 
       initializer "decidim_solutions.assets" do |app|
-        app.config.assets.precompile += %w(decidim_challenges_manifest.js decidim_challenges_manifest.css)
+        app.config.assets.precompile += %w(decidim_solutions_manifest.js decidim_solutions_manifest.css)
       end
 
       initializer "decidim_challenges.add_cells_view_paths" do
