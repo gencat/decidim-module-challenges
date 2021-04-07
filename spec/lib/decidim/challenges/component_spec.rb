9c1b2ed76dfc035bb9fafd3fde41e451da3da0ae
Laura Jaime <laura.jv@coditramuntana.com>
2021-04-07 11:20:32 +0200

    Challenges: Add new setting for hide filters
    * Add settings
    * Changes in public views
    * Add system and component specs

---
+++ spec/lib/decidim/challenges/component_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Challenges component" do # rubocop:disable RSpec/DescribeClass
+  subject { component }
+
+  let(:component) { create :challenges_component }
+
+  context "when check filters are hide in settings" do
+    before do
+      component.settings = { hide_filters: true }
+    end
+
+    it "save correctly component" do
+      expect(component.save!).to be true
+    end
+
+    it "has correct settings" do
+      expect(component.settings.hide_filters).to be true
+    end
+  end
+end
---
+++ spec/system/decidim/challenges/without_filters_challenges_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Without filters Challenges", :slow, type: :system do
+  include_context "with a component"
+  let(:manifest_name) { "challenges" }
+
+  let!(:category) { create :category, participatory_space: participatory_process }
+  let!(:scope) { create :scope, organization: organization }
+  let!(:user) { create :user, :confirmed, organization: organization }
+  let(:scoped_participatory_process) { create(:participatory_process, :with_steps, organization: organization, scope: scope) }
+
+  describe "when filters are hide" do
+    let(:scopes_picker) { select_data_picker(:filter_scope_id, multiple: true, global_value: "global") }
+    let!(:scope_2) { create :scope, organization: participatory_process.organization }
+
+    before do
+      component.settings = { hide_filters: true }
+      component.save!
+      create_list(:challenge, 2, component: component, scope: scope)
+      create(:challenge, component: component, scope: scope_2)
+      create(:challenge, component: component, scope: nil)
+      visit_component
+    end
+
+    it "show challenges in three columns" do
+      expect(page).to have_css(".mediumlarge-11.large-12")
+    end
+
+    it "not show filters" do
+      expect(page).not_to have_css(".filters")
+    end
+  end
+end
