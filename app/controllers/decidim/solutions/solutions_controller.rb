f62a418115c3e319946e1e49718083cb85e8dda8
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2021-05-11 12:48:09 +0200

    Rubocopify

--- app/controllers/decidim/solutions/solutions_controller.rb
+++ app/controllers/decidim/solutions/solutions_controller.rb
@@ -72,10 +72,10 @@ module Decidim
 
       def challenge_scope
         @challenge_scope ||= if @solution.problem.present?
-                              current_organization.scopes.find_by(id: @solution.problem.challenge.decidim_scope_id)
-                            else
-                              current_organization.scopes.find_by(id: @solution.challenge.decidim_scope_id)
-                            end
+                               current_organization.scopes.find_by(id: @solution.problem.challenge.decidim_scope_id)
+                             else
+                               current_organization.scopes.find_by(id: @solution.challenge.decidim_scope_id)
+                             end
       end
 
       def sectorial_scope
