a5d35511613fa6c46ae2f18d473b460fa37e371d
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2022-08-24 18:14:05 +0200

    TEST: Fix tests for searchables

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,7 @@
 
 require "decidim/forms/test/factories"
 require "decidim/challenges/test/factories"
+require "decidim/challenges/test/shared_examples/unreportable_searchable_results_examples"
 require "decidim/problems/test/factories"
 require "decidim/sdgs/test/factories"
 require "decidim/solutions/test/factories"
