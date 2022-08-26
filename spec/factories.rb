7023e1371ef974f9e3343e35937ccfc38f545609
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2022-08-26 15:06:45 +0200

    TEST: improve performance by splitting

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,6 @@
 
 require "decidim/forms/test/factories"
 require "decidim/challenges/test/factories"
-require "decidim/challenges/test/shared_examples/unreportable_searchable_results_examples"
 require "decidim/problems/test/factories"
 require "decidim/sdgs/test/factories"
 require "decidim/solutions/test/factories"
