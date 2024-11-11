3383218b8be9f03c104c382e47cd50cfb7ef1173
Laura Jaime <laura.jv@coditramuntana.com>
2024-11-11 09:22:04 +0100

    Some rework in challenges

--- app/helpers/decidim/challenges/application_helper.rb
+++ app/helpers/decidim/challenges/application_helper.rb
@@ -11,6 +11,26 @@ module Decidim
         i18n_key = "decidim.components.challenges.name"
         (defined?(current_component) && translated_attribute(current_component&.name).presence) || t(i18n_key)
       end
+
+      def filter_sdgs_values
+        Decidim::CheckBoxesTreeHelper::TreeNode.new(
+          Decidim::CheckBoxesTreeHelper::TreePoint.new("", t("decidim.challenges.challenges_helper.filter_state_values.all")),
+          Decidim::Sdgs::Sdg::SDGS.map do |sdg_code|
+            Decidim::CheckBoxesTreeHelper::TreePoint.new(sdg_code, I18n.t("#{sdg_code}.objectives.subtitle", scope: "decidim.components.sdgs"))
+          end
+        )
+      end
+
+      def filter_custom_state_values
+        Decidim::CheckBoxesTreeHelper::TreeNode.new(
+          Decidim::CheckBoxesTreeHelper::TreePoint.new("", t("decidim.challenges.challenges_helper.filter_state_values.all")),
+          [
+            Decidim::CheckBoxesTreeHelper::TreePoint.new("proposal", t("decidim.challenges.challenges_helper.filter_state_values.proposal")),
+            Decidim::CheckBoxesTreeHelper::TreePoint.new("execution", t("decidim.challenges.challenges_helper.filter_state_values.execution")),
+            Decidim::CheckBoxesTreeHelper::TreePoint.new("finished", t("decidim.challenges.challenges_helper.filter_state_values.finished")),
+          ]
+        )
+      end
     end
   end
 end
--- app/helpers/decidim/challenges/challenges_helper.rb
+++ app/helpers/decidim/challenges/challenges_helper.rb
@@ -8,34 +8,14 @@ module Decidim
       def filter_sections
         if has_sdgs
           [
-            { method: :with_any_state, collection: filter_challenges_state_values, label_scope: "decidim.shared.filters", id: "state" },
+            { method: :with_any_state, collection: filter_custom_state_values, label_scope: "decidim.shared.filters", id: "state" },
             { method: :with_any_sdgs_codes, collection: filter_sdgs_values, label_scope: "decidim.shared.filters", id: "sdgs" },
           ].reject { |item| item[:collection].blank? }
         else
           [
-            { method: :with_any_state, collection: filter_challenges_state_values, label_scope: "decidim.shared.filters", id: "state" },
+            { method: :with_any_state, collection: filter_custom_state_values, label_scope: "decidim.shared.filters", id: "state" },
           ].reject { |item| item[:collection].blank? }
         end
-      end
-
-      def filter_challenges_state_values
-        Decidim::CheckBoxesTreeHelper::TreeNode.new(
-          Decidim::CheckBoxesTreeHelper::TreePoint.new("", t("decidim.challenges.challenges_helper.filter_state_values.all")),
-          [
-            Decidim::CheckBoxesTreeHelper::TreePoint.new("proposal", t("decidim.challenges.challenges_helper.filter_state_values.proposal")),
-            Decidim::CheckBoxesTreeHelper::TreePoint.new("execution", t("decidim.challenges.challenges_helper.filter_state_values.execution")),
-            Decidim::CheckBoxesTreeHelper::TreePoint.new("finished", t("decidim.challenges.challenges_helper.filter_state_values.finished")),
-          ]
-        )
-      end
-
-      def filter_sdgs_values
-        Decidim::CheckBoxesTreeHelper::TreeNode.new(
-          Decidim::CheckBoxesTreeHelper::TreePoint.new("", t("decidim.challenges.challenges_helper.filter_state_values.all")),
-          Decidim::Sdgs::Sdg::SDGS.map do |sdg_code|
-            Decidim::CheckBoxesTreeHelper::TreePoint.new(sdg_code, I18n.t("#{sdg_code}.objectives.subtitle", scope: "decidim.components.sdgs"))
-          end
-        )
       end
 
       def challenge_associated_solutions(challenge)
