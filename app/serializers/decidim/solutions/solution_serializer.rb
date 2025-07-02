23f7da1c5b4b2f1a487d0c8245fd9c325803e681
Lucas <lucas.gv@coditramuntana.com>
2025-07-02 09:12:13 +0200

    fix version rubocop-factory_bot and refactor serializer

--- app/serializers/decidim/solutions/solution_serializer.rb
+++ app/serializers/decidim/solutions/solution_serializer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Decidim
   module Solutions
     class SolutionSerializer < Decidim::Exporters::Serializer
@@ -13,13 +15,13 @@ module Decidim
 
       def serialize
         {
-          "#{I18n.t('export.title', scope: 'decidim.solutions.admin.exports')}" => @solution.title[I18n.locale.to_s],
-          "#{I18n.t('export.description', scope: 'decidim.solutions.admin.exports')}" => sanitize(@solution.description[I18n.locale.to_s]),
-          "#{I18n.t('export.status', scope: 'decidim.solutions.admin.exports')}" => @solution&.project_status,
-          "#{I18n.t('export.challenge', scope: 'decidim.solutions.admin.exports')}" => @solution&.challenge&.title.present? ? @solution&.challenge&.title[I18n.locale.to_s] : "",
-          "#{I18n.t('export.url', scope: 'decidim.solutions.admin.exports')}" => @solution&.project_url,
-          "#{I18n.t('export.created_at', scope: 'decidim.solutions.admin.exports')}" => @solution.created_at,
-          "#{I18n.t('export.published_at', scope: 'decidim.solutions.admin.exports')}" => @solution&.published_at
+          title_label => @solution.title[I18n.locale.to_s],
+          description_label => sanitized_description,
+          status_label => @solution&.project_status,
+          challenge_label => translated_challenge_title,
+          url_label => @solution&.project_url,
+          created_at_label => @solution.created_at,
+          published_at_label => @solution&.published_at,
         }
       end
 
@@ -30,6 +32,42 @@ module Decidim
 
       def sanitize(text)
         ActionController::Base.helpers.strip_tags(text)
+      end
+
+      def title_label
+        I18n.t("export.title", scope: "decidim.solutions.admin.exports").to_s
+      end
+
+      def description_label
+        I18n.t("export.description", scope: "decidim.solutions.admin.exports").to_s
+      end
+
+      def status_label
+        I18n.t("export.status", scope: "decidim.solutions.admin.exports").to_s
+      end
+
+      def challenge_label
+        I18n.t("export.challenge", scope: "decidim.solutions.admin.exports").to_s
+      end
+
+      def url_label
+        I18n.t("export.url", scope: "decidim.solutions.admin.exports").to_s
+      end
+
+      def created_at_label
+        I18n.t("export.created_at", scope: "decidim.solutions.admin.exports").to_s
+      end
+
+      def published_at_label
+        I18n.t("export.published_at", scope: "decidim.solutions.admin.exports").to_s
+      end
+
+      def translated_challenge_title
+        @solution&.challenge&.title.present? ? @solution&.challenge&.title&.[](I18n.locale.to_s) : ""
+      end
+
+      def sanitized_description
+        sanitize(@solution.description[I18n.locale.to_s])
       end
     end
   end
